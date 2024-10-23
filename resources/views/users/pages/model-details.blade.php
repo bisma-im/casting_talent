21e92c8ef7985634ba5e1d8383f72443b590bc48
Bisma Imran <bismaimran36@gmail.com>
2024-10-24 01:36:53 +0500

    model details section 1 final

--- resources/views/users/pages/model-details.blade.php
+++ resources/views/users/pages/model-details.blade.php
@@ -7,14 +7,9 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
 <style>
     /* Header styling */
-    .h-user-info p {
-        font-size: 13px;
-        text-align: left;
-    }
-
     .header {
         /* padding: 1em; */
-        color: #d9480f;
+        /* color: #d9480f; */
         /* border-bottom: 2px solid rgb(233, 171, 88); */
     }
 
@@ -153,28 +148,43 @@
         /* Full width of the parent container or viewport */
         font-family: Arial, sans-serif;
         box-sizing: border-box;
-        padding: 20px;
+        /* padding: 20px; */
     }
 
-    .user-info-row {
+    /* .user-info-row {
         display: flex;
         justify-content: space-between;
         padding: 7px;
         width: 100%;
-        /* Make each row span the full width of the parent */
         box-sizing: border-box;
-        /* Ensure padding is included in the width */
         font-size: 14px;
+        position: relative;
     }
 
-    .user-info-row:nth-child(odd) {
-        background-color: #DAD7B1;
-        /* Base color */
+    .label {
+        font-weight: bold;
+        color: #333;
     }
 
-    .user-info-row:nth-child(even) {
-        background-color: #CFCBA1;
-        /* Slightly darker shade for alternate row */
+    .info {
+        text-align: right;
+        color: #555;
+    } */
+
+    .container {
+        display: flex;
+        align-items: center;
+        /* Centers children vertically */
+    }
+
+    .user-info-row {
+        flex-grow: 1;
+        /* Allows it to grow and take up any remaining space */
+        display: flex;
+        justify-content: space-between;
+        padding: 7px;
+        box-sizing: border-box;
+        font-size: 14px;
     }
 
     .label {
@@ -187,8 +197,82 @@
         color: #555;
     }
 
+    .container>p {
+        flex: 0 0 50px;
+        margin: 0 20px 0 5px;
+        display: flex;
+        font-size: 12px;
+        align-items: center;
+        /* Centers text vertically */
+        justify-content: center;
+        /* Centers text horizontally */
+    }
+
+
+
+    .row .container:nth-child(odd) .user-info-row {
+        background-color: #DAD7B1;
+        /* Lighter color for odd rows */
+    }
+
+    .row .container:nth-child(even) .user-info-row {
+        background-color: #CFCBA1;
+        /* Slightly darker color for even rows */
+    }
+
     .user-info-row i {
         margin-right: 5px;
+    }
+
+    .btn.icon-btn {
+        background: none;
+        /* Removes the background */
+        border: none;
+        /* Removes the border */
+        color: inherit;
+        /* Takes the color from the parent or you can set a specific color */
+        padding: 5px;
+        /* Minimal padding to increase the clickable area, adjust as needed */
+        cursor: pointer;
+        /* Ensures the cursor changes to a pointer to indicate it's a clickable item */
+    }
+
+    .btn.icon-btn:hover,
+    .btn.icon-btn:focus {
+        background-color: transparent;
+        /* Ensures that hovering or focusing doesn't change the background */
+        outline: none;
+        /* Removes focus outline, you may want to replace this with a more accessible option */
+    }
+
+    .btn.icon-btn i {
+        color: #333;
+        /* Set the color of the icon, adjust as per your design */
+    }
+
+    .bg-cfcba1 {
+        background-color: #CFCBA1;
+        /* Light grayish-brown color */
+    }
+
+    .bottom-p {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        /* Centers content vertically in the container */
+        text-align: center;
+        /* Aligns text horizontally */
+        height: 100%;
+    }
+
+    .h-user-info p {
+        font-size: 12px;
+        /* Sets font size */
+        text-align: center;
+        /* Ensures text is centered horizontally */
+        margin: 0;
+        /* Removes any default margin */
+        padding: 8px 0;
     }
 </style>
 
@@ -250,122 +334,114 @@ $timestamp = time();
         {{-- ------------------ model details -------------------- --}}
         <div class="col-md-7 p-0">
             <div class="col-md-6 user-info">
-                {{-- <div class="user-info-row">
-                    <span class="label"><i class="fas fa-flag"></i>Nationality</span>
-                    <span class="info">{{ $details['nationality'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-language"></i>Languages Spoken</span>
-                    <span class="info">{{ implode(', ', json_decode($details['languages_spoken'], true)) }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-map-marker-alt"></i>Location</span>
-                    <span class="info">{{ $details['location'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-passport"></i>Visa Status</span>
-                    <span class="info"></span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-id-card"></i>Driving License</span>
-                    <span class="info">{{ $details['driving_license'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-pen"></i>Tattoos</span>
-                    <span class="info">{{ $details['have_tattos'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-eye"></i>Eye Color</span>
-                    <span class="info">{{ $details['eye_color'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-tint"></i>Hair Color</span>
-                    <span class="info">{{ $details['hair_color'] }}</span>
-                </div>
-                <div class="user-info-row">
-                    <span class="label"><i class="fas fa-cut"></i>Hair Length</span>
-                    <span class="info">{{ $details['hair_length'] }}</span>
-                </div> --}}
-                <div class="row px-5">
-                    <div class="header p-3">
+                <div class="row ps-4">
+                    <div class="header pe-5 py-3">
                         <div class="name">
                             <!--{{ $details['first_name'] }} {{ $details['last_name'] }} => -->
                             <span>CTF-00001</span>
+                            {{--
                             <!--{{ route('download.model.details', $details['id']) }} {{ url('/download-all-model-images/' . $details['id']) }}  id="captureButton" -->
-                            <button id="captureButton" class="btn btn-success">Download</button>
+                            <button id="captureButton" class="btn btn-success">Download</button> --}}
+                            <div>
+                                <button id="captureButton" class="btn icon-btn"><i class="fas fa-share"></i></button>
+                                <button id="captureButton1" class="btn icon-btn"><i
+                                        class="fad fa-print"></i></button>
+                            </div>
                         </div>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-arrows-alt-v"></i>Height</span>
-                        <span class="info">{{ $details['height'] }} cm</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-arrows-alt-v"></i>Height</span>
+                            <span class="info">{{ $details['height'] }}</span>
+                        </div>
+                        <p>CM</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-ruler-vertical"></i>Bust</span>
-                        <span class="info">{{ $details['bust'] }} cm</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-ruler-vertical"></i>Bust</span>
+                            <span class="info">{{ $details['bust'] }}</span>
+                        </div>
+                        <p>CM</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-ruler-horizontal"></i>Hip</span>
-                        <span class="info">{{ $details['hip'] }} cm</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-ruler-horizontal"></i>Hip</span>
+                            <span class="info">{{ $details['hip'] }}</span>
+                        </div>
+                        <p>CM</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-tshirt"></i>Dress Size</span>
-                        <span class="info">{{ $details['dress_size'] }} cm</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-tshirt"></i>Dress Size</span>
+                            <span class="info">{{ $details['dress_size'] }}</span>
+                        </div>
+                        <p>EURO</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-ruler-combined"></i> Waist:</span>
-                        <span class="info">{{ $details['waist'] }} cm</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-ruler-combined"></i> Waist:</span>
+                            <span class="info">{{ $details['waist'] }}</span>
+                        </div>
+                        <p>CM</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-shoe-prints"></i>Shoe Size</span>
-                        <span class="info">{{ $details['shoe_size'] }}</span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-shoe-prints"></i>Shoe Size</span>
+                            <span class="info">{{ $details['shoe_size'] }}</span>
+                        </div>
+                        <p>EURO</p>
                     </div>
-                    <div class="user-info-row">
-                        <span class="label"><i class="fas fa-tape"></i>Pants Size</span>
-                        <span class="info"></span>
+                    <div class="container">
+                        <div class="user-info-row">
+                            <span class="label"><i class="fas fa-tape"></i>Pants Size</span>
+                            <span class="info"></span>
+                        </div>
+                        <p>EURO</p>
                     </div>
                 </div>
-                <div class=" ms-md-4 ">
+                <div class="mt-md-2 ms-md-3 me-md-3 mb-md-3">
                     <div class="row pt-3 ps-3 pe-3 mt-2 h-user-info">
-                        <div class="col-md-4">
-                            <p><strong>Nationality: </strong> {{ $details['nationality'] }}
+                        <div class="col-md-4 bg-cfcba1 bottom-p">
+                            <p class="m-0"><strong>Nationality: </strong><br />{{ $details['nationality'] }}
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Languages: </strong>{{ implode(', ', json_decode($details['languages_spoken'],
+                        <div class="col-md-4 bottom-p">
+                            <p><strong>Languages: </strong><br />{{ implode(', ',
+                                json_decode($details['languages_spoken'],
                                 true))
                                 }}
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Location: </strong> {{ $details['location'] }}
+                        <div class="col-md-4 bg-cfcba1 bottom-p">
+                            <p><strong>Location: </strong><br />{{ $details['location'] }}
                             </p>
                         </div>
                     </div>
                     <div class="row ps-3 pe-3 text-center h-user-info">
-                        <div class="col-md-4">
-                            <p><strong>Visa Status: </strong>Golden
+                        <div class="col-md-4 bottom-p">
+                            <p><strong>Visa Status: </strong><br />Golden
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Driving License: </strong> {{ $details['driving_license'] }}
+                        <div class="col-md-4 bg-cfcba1 bottom-p">
+                            <p><strong>Driving License: </strong><br />{{ $details['driving_license'] }}
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Tattoos: </strong> {{ $details['have_tattos'] }}
+                        <div class="col-md-4 bottom-p">
+                            <p><strong>Tattoos: </strong><br />{{ $details['have_tattos'] }}
                             </p>
                         </div>
                     </div>
                     <div class="row ps-3 pe-3 text-center h-user-info">
-                        <div class="col-md-4">
-                            <p><strong>Eye Color: </strong>{{ $details['eye_color'] }}
+                        <div class="col-md-4 bg-cfcba1 bottom-p">
+                            <p><strong>Eye Color: </strong><br />{{ $details['eye_color'] }}
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Hair Color: </strong> {{ $details['hair_color'] }}
+                        <div class="col-md-4 bottom-p">
+                            <p><strong>Hair Color: </strong><br />{{ $details['hair_color'] }}
                             </p>
                         </div>
-                        <div class="col-md-4">
-                            <p><strong>Hair Length: </strong> {{ $details['hair_length'] }}
+                        <div class="col-md-4 bg-cfcba1 bottom-p">
+                            <p><strong>Hair Length: </strong><br />{{ $details['hair_length'] }}
                             </p>
                         </div>
                     </div>
