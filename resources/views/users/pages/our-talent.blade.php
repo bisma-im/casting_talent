b3c0fc8e41ebc3a0c8b7f10910a4022a3cedb063
MuhammadHuzaifa12356 <huzaifajt2525@gmail.com>
2024-11-26 01:38:51 +0500

    images updated

--- resources/views/users/pages/our-talent.blade.php
+++ resources/views/users/pages/our-talent.blade.php
@@ -204,7 +204,7 @@
         left: 0;
         right: 0;
         bottom: 0;
-        background-image: url('{{ url('/user-assets/images/videophoto.avif') }}');
+        background-image: url('{{ url('/user-assets/images/videophoto.webp') }}');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -247,7 +247,7 @@
         left: 0;
         right: 0;
         bottom: 0;
-        background-image: url('{{ url('/user-assets/images/filmcrew.webp') }}');
+        background-image: url('{{ url('/user-assets/images/filmcrew.jpeg') }}');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -423,7 +423,7 @@
         left: 0;
         right: 0;
         bottom: 0;
-        background-image: url('{{ url('/user-assets/images/Influencers.webp') }}');
+        background-image: url('{{ url('/user-assets/images/Influencers.jpeg') }}');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
