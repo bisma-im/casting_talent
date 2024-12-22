200140ffbb583ebe131684b6984c43f4a9049c2e
MuhammadHuzaifa12356 <huzaifajt2525@gmail.com>
2024-12-23 01:34:43 +0500

    logo

--- resources/views/users/includes/footer.blade.php
+++ resources/views/users/includes/footer.blade.php
@@ -832,7 +832,7 @@
                         <!--</div>-->
                     </div> --}}
                     <div class="owl-carousel owl-theme mt-5" id="testimonailSliderff">
-                        @for ($i = 1; $i <= 45; $i++)
+                        @for ($i = 1; $i <= 44; $i++)
                             <div class="item">
                                 <div class="image-container">
                                     <img src="{{ url('user-assets/loggs/' . $i . '.png') }}" class="img-fluid" alt="Image {{ $i }}" style="width: 80px; height: 110px;">
