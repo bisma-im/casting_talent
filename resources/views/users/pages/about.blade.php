42e9273ae4fa546863bfe1da5d3bfb3ca3478492
Bisma Imran <bismaimran36@gmail.com>
2025-01-17 20:47:39 +0500

    about us

--- resources/views/users/pages/about.blade.php
+++ resources/views/users/pages/about.blade.php
@@ -161,9 +161,9 @@
                         <p>At Cast Talents LLC, our team embodies a blend of creativity, professionalism, and deep expertise, dedicated to transforming visions into captivating realities. Each member of our team brings unique talents and a wealth of experience in the modeling and entertainment industry, ensuring that we deliver exceptional services tailored to each client’s needs.</p>
                     </div>
 
-                    <div class="row pt-5">
+                    <div class="row pt-5 d-flex align-items-stretch">
                         <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="teambox">
+                            <div class="teambox teambox-founder">
                                 <div class="my-teamimg">
                                     <img  data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500" src="{{ url('user-assets') }}/team-images/abeera.jpg" class="img-fluid" alt="img">
                                 </div>
@@ -184,86 +184,90 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <div class="row">
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/aneela.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Aneela Iftikhar</h3>
-                                            <h4>Principal Accountant</h4>
-                                            <p>Leading Financial Integrity with Expertise and Precision.</p>
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6 teambox-row">
+                                <div class="row d-flex align-items-start justify-content-between">
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/aneela.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Aneela Iftikhar</h3>
+                                                <h4>Principal Accountant</h4>
+                                                <p>Leading Financial Integrity with Expertise and Precision.</p>
 
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/fayzan.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Fayzan Mustak</h3>
-                                            <h4>Event Specialist</h4>
-                                            <p>Bringing Events to Life with Precision and Passion.</p>
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/fayzan.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Fayzan Mustak</h3>
+                                                <h4>Event Specialist</h4>
+                                                <p>Bringing Events to Life with Precision and Passion.</p>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/ira.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Ira Kovalenko</h3>
-                                            <h4>Marketing Manager</h4>
-                                            <p>Innovative Strategies for Maximum Impact.</p>
+                                <div class="row d-flex align-items-start justify-content-between">
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/ira.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Ira Kovalenko</h3>
+                                                <h4>Marketing Manager</h4>
+                                                <p>Innovative Strategies for Maximum Impact.</p>
 
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/safder-ali.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Safder Ali</h3>
-                                            <h4>Senior DOP</h4>
-                                            <p>From Vision to Video: Perfecting Every Frame.</p>
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/safder-ali.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Safder Ali</h3>
+                                                <h4>Senior DOP</h4>
+                                                <p>From Vision to Video: Perfecting Every Frame.</p>
 
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/reza.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Reza Kalamdeen</h3>
-                                            <h4>Head of Location Scouting / Permits</h4>
-                                            <p>Unearthing Hidden Gems for Memorable Productions.</p>
+                                <div class="row d-flex align-items-start justify-content-between">
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/reza.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Reza Kalamdeen</h3>
+                                                <h4>Head of Location Scouting / Permits</h4>
+                                                <p>Unearthing Hidden Gems for Memorable Productions.</p>
 
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6 teambox-col pb-3">
+                                        <div class="teambox teambox-team">
+                                            <div class="teamimg">
+                                                <img src="{{ url('user-assets') }}/team-images/raziq.jpg" class="img-fluid" alt="img">
+                                            </div>
+                                            <div class="teambody">
+                                                <h3>Raziq Shah</h3>
+                                                <h4>Line Producer</h4><br/>
+                                                <p>From Planning to Wrap – We’ve Got You Covered.</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 col-sm-12 col-lg-6 col-xl-6 col-xxl-6">
-                                    <div class="teambox">
-                                        <div class="teamimg">
-                                            <img src="{{ url('user-assets') }}/team-images/raziq.jpg" class="img-fluid" alt="img">
-                                        </div>
-                                        <div class="teambody">
-                                            <h3>Raziq Shah</h3>
-                                            <h4>Line Producer</h4><br/>
-                                            <p>From Planning to Wrap – We’ve Got You Covered.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
