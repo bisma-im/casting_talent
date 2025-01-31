e8eeffda6af31d469f7327fe49a868abcb66cc46
MuhammadHuzaifa12356 <huzaifajt2525@gmail.com>
2025-01-31 08:39:06 +0500

    alingment set

--- resources/views/users/pages/filming-services.blade.php
+++ resources/views/users/pages/filming-services.blade.php
@@ -100,39 +100,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     <section class="modalagencysec2" id="filmmaking-equipment">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <div class="modalimg">
-                        <img src="{{ url('user-assets') }}/images/filim_2.jpg" alt="Casting Image" class="img-fluid">
-                    </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="modalimg">
+                    <img src="{{ url('user-assets') }}/images/filim_2.jpg" alt="Casting Image" class="img-fluid">
                 </div>
-                <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <div class="modaltext1">
-
-                        <h2>Film Making <sapn style="color: rgba(216, 31, 38, 1);">Equipment </sapn>
-                        </h2>
-                        <p><b>Comprehensive Production Equipment Services by Cast Talents</b></p>
-                        <p>At Cast Talents, we bring your vision to life from start to finish. With our expertise and
-                            reliability, we ensure seamless execution, paying meticulous attention to every detail.</p>
-                        <p>Explore our extensive range of high-quality equipment:</p>
-                        <ul>
-                            <li>Cameras</li>
-                            <li>Lenses</li>
-                            <li>Tripods</li>
-                            <li>Sound and Audio Equipment</li>
-                            <li>Lighting Equipment</li>
-                            <li>Aerial Drone Filming</li>
-                        </ul>
-                        <p>Count on Cast Talents to provide the tools you need for a successful production, enhancing every
-                            aspect of your project with precision and professionalism.</p>
-
-                        <!--<a href="#" class="contactbtn1">READ MORE</a>-->
-                    </div>
+            </div>
+            <div class="col-md-6">
+                <div class="modaltextEquipment">
+                    <h2>Film Making <span>Equipment</span></h2>
+                    <p><b>Comprehensive Production Equipment Services by Cast Talents</b></p>
+                    <p>At Cast Talents, we bring your vision to life from start to finish. With our expertise and reliability, we ensure seamless execution, paying meticulous attention to every detail.</p>
+                    <p>Explore our extensive range of high-quality equipment:</p>
+                    <ul>
+                        <li>Cameras</li>
+                        <li>Lenses</li>
+                        <li>Tripods</li>
+                        <li>Sound and Audio Equipment</li>
+                        <li>Lighting Equipment</li>
+                        <li>Aerial Drone Filming</li>
+                    </ul>
+                    <p>Count on Cast Talents to provide the tools you need for a successful production, enhancing every aspect of your project with precision and professionalism.</p>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
     <section class="modalagencysec" id="production">
         <div class="container">
